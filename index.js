363910d34a77cd5b8488ec10a84bdd639830154e
Angel Illescas <angelalfonso.illescas@gmail.com>
2022-04-24 00:55:29 -0500

    Primer ejercicio

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionComander');
+const carlo = new MissionCommander("Carlo")
+const martin = new MissionCommander("Martin")
+const alfredo = new MissionCommander("Alfredo")
+
+console.log(carlo.name)
+console.log(martin.name)
+console.log(alfredo.name)
---
+++ test/missionCommander.test.js
@@ -0,0 +1,20 @@
+const MissionCommander = require('../app/missionComander');
+
+describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander objet', () => {
+        const myMissionCommander = new MissionCommander("Woopa")
+        expect(myMissionCommander.name).toBe("Woopa");
+    })
+});
+
+
+
+// Example
+
+/* describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+      const result = 1 + 2 
+      expect(result).toBe(3);
+    });
+  }) */
+  
